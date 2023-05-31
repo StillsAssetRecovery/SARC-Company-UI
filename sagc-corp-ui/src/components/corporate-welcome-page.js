140f2d42f5f3563c5d5a72c27ba39fc41c756a4a
Montaniz.Stills <montanizstills@gmail.com>
2023-05-31 00:21:54 -0400

    feat(ui-design): implement wix template;

--- sagc-corp-ui/src/components/corporate-welcome-page.js
+++ sagc-corp-ui/src/components/corporate-welcome-page.js
@@ -1,12 +1,34 @@
-import React, { useEffect, useState, useRef } from "react";
+import React from "react";
 import * as ReactBootStrap from "react-bootstrap";
-import "../styles/corporate-welcome-page.css"
+// import "../styles/corporate-welcome-page.css";
+import myImage from "../assets/new_york_city_view.png"
 
 function CorportateWelcomePage() {
     return (
         <div id="page_container">
-            <HeaderNavbarElements />
-            <div class="justify-content-left">I am a image after the NavBar</div>
+            <div id="logo_headerNavbar_container" style={{ display: "flex", flexDirection: "row", justifyContent: "center", padding: "0em 0em 0em 0em" }}>
+                <div id="logo_container">Our Logo</div>
+                <div id="headerNavbar_container"><HeaderNavbarElements /></div>
+            </div>
+            <div>
+                <div style={{ display: "flex", justifyContent: "center", width: "80%" }}>
+                    <div class="justify-content-left"><ReactBootStrap.Image src={myImage}></ReactBootStrap.Image></div>
+                    <div><p style={{ fontSize: "1.25em" }}>Powering you through Data-Driven-Solutions</p></div>
+                </div>
+                <div>
+                    <div><p style={{ color: "#1A2B6D", fontSize: "2em" }}>Turning Completxity, into Simplicity</p></div>
+                    <div><p>With a wealth of experience and a deep understanding of the industry, we are dedicated to helping our clients achieve their financial goals and build a secure future. Whether you are an individual investor or a large corporation, we have the knowledge and resources to help you succeed.</p></div>
+                </div>
+                <div style={{backgroundColor: "#F1A8A9"}}>
+                    <div><p style={{ fontSize: "1.5em" }}>Our Services</p></div>
+                    <div><p style={{ fontSize: "1.1em" }}>Technology Solutions</p><p>Describe one of your services</p></div>
+                    <div><p style={{ fontSize: "1.1em" }}>Corporate Financial Planning</p><p>Describe one of your services</p></div>
+                    <div><p style={{ fontSize: "1.1em" }}>Digital Marketing Services</p><p>Describe one of your services</p></div>
+                    <div><p style={{ fontSize: "1.1em" }}>Lead Generation</p><p>Describe one of your services</p></div>
+                    <div><p style={{ fontSize: "1.1em" }}>Bidding (RFP) Consulting</p><p>Describe one of your services</p></div>
+                </div>
+            </div>
+
         </div>
     );
 }
@@ -41,11 +63,9 @@ const HeaderNavbarElements = () => {
                 if (element.dropdownItems) {
                     return (
                         <ReactBootStrap.NavDropdown title={element.name} key={element.name} autoClose={true}>
-                            {element.dropdownItems.map((item) => {
-                                return (
-                                    <ReactBootStrap.NavDropdown.Item key={item.name}>{item.name}</ReactBootStrap.NavDropdown.Item>
-                                )
-                            })}
+                            {element.dropdownItems.map(
+                                (item) => { return <ReactBootStrap.NavDropdown.Item style={{ padding: "0em 5em 0em 5em" }} key={item.name}>{item.name}</ReactBootStrap.NavDropdown.Item> }
+                            )}
                         </ReactBootStrap.NavDropdown>
                     )
                 } else {
